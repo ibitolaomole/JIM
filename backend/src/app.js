e179c55568ef5d1ecd547e405779d3b3342edf9a
yourgirlsimdi <chisimdi.eso@gmail.com>
2026-05-09 17:28:53 +0100

    Updated with comments

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,21 @@ app.use("/api", healthRoutes);
 module.exports = app;
 
 
+
+
+
+
+//TODO
+//POST - Get the ppt file from the front-end
+//express creates the route and mutler handles uoloads
+//POST - Send the ppt file to the gemini in backend
+//GET - Get the generated questions from the gemini in backend
+//POST - Send the generated questions to the front-end
+//GET - Get the generated story from the gemini in backend
+//POST - Send the generated story to the front-end
+
+
+
 //middleware for parsing json
 const upload = multer({ dest: "uploads/" });
 
